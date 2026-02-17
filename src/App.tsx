470caefa80b26248e3f75057be75b273b68421b3
Amer <amerzakret223@gmail.com>
2026-02-17 09:50:19 +0300

    v1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı Ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Amer Zakret</p>
+      <p>No: 240541625</p>
+    </div>
+  )
+}
+
+export default App
